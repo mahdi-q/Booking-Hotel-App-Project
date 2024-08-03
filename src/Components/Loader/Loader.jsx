27765980c8c698adb34bd0e00bf4958a34663bb8
Mahdi-Ghasemi <ghasemi84mahdi@gmail.com>
2024-08-03 09:41:39 +0330

    Updating styles of Loader component

--- src/Components/Loader/Loader.jsx
+++ src/Components/Loader/Loader.jsx
@@ -1,5 +1,21 @@
+import { LoaderIcon } from "react-hot-toast";
+
 function Loader() {
-  return <p style={{ fontWeight: "bold" }}>LOADING ...</p>;
+  return (
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        gap: "1rem",
+        margin: "1rem auto",
+        fontWeight: "bold",
+        color: "var(--primary-600)",
+      }}
+    >
+      <p>Loading Data ...</p>
+      <LoaderIcon style={{ width: "1.3rem", height: "1.3rem" }} />
+    </div>
+  );
 }
 
 export default Loader;
