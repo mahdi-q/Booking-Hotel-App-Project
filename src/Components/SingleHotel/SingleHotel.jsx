52f08cbe4d173989fc56d09d2abb7a6e50feca50
Mahdi-Ghasemi <ghasemi84mahdi@gmail.com>
2024-08-13 14:32:23 +0330

    Update SingleHotel component

--- src/Components/SingleHotel/SingleHotel.jsx
+++ src/Components/SingleHotel/SingleHotel.jsx
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 import { useHotels } from "../../Contexts/HotelsContext";
 import Loader from "../Loader/Loader";
 import { useNavigate, useParams } from "react-router-dom";
-import { HiChevronDoubleLeft } from "react-icons/hi";
+import { HiChevronDoubleLeft, HiOutlineClock } from "react-icons/hi";
 
 function SingleHotel() {
   const { id } = useParams();
@@ -20,19 +20,34 @@ function SingleHotel() {
 
   return (
     <div className="room">
+      <button className="btn btn--back" onClick={() => navigate(-1)}>
+        <HiChevronDoubleLeft /> Back
+      </button>
       <div className="roomDetail">
-        <button className="btn btn--back" onClick={() => navigate(-1)}>
-          <HiChevronDoubleLeft /> Back
-        </button>
-
-        <h2>{currentHotel.name}</h2>
-
-        <div>
-          {currentHotel.number_of_reviews} reviews &bull;{" "}
-          {currentHotel.smart_location}
-        </div>
-
         <img src={currentHotel.xl_picture_url} alt={currentHotel.name} />
+
+        <div className="roomInfo">
+          <h2 className="name">{currentHotel.name}</h2>
+
+          <div className="review">
+            <strong>{currentHotel.number_of_reviews}</strong> reviews &bull;{" "}
+            {currentHotel.smart_location}
+          </div>
+
+          <div className="type">
+            {currentHotel.property_type} &bull; {currentHotel.room_type} &bull;{" "}
+            {currentHotel.bed_type}
+          </div>
+
+          <p className="price">
+            <strong>{currentHotel.price}&nbsp;€</strong>&nbsp;<span>night</span>
+          </p>
+
+          <div className="update">
+            <HiOutlineClock />
+            {currentHotel.calendar_updated}&nbsp;updated
+          </div>
+        </div>
       </div>
     </div>
   );
